417ecd82c6f3aea1583a421defb2ccfdd6919a74
cerpeng <cerpeng@hotmail.com>
2017-01-04 10:06:56 +0800

    update: 格式化代码

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,42 +1,37 @@
 package controllers;
 
 import com.alibaba.fastjson.JSON;
-import com.google.gson.JsonObject;
 import models.User;
-import play.Play;
-import play.data.validation.Required;
 import play.mvc.Controller;
 
-/**
- * Created by cerpengxi on 17/1/3.
- */
-
+/** Created by cerpengxi on 17/1/3. */
 public class Users extends Controller {
-    public static void add(String body) {
-        User input = JSON.parseObject(body, User.class);
-        User user = new User();
-        user.name = input.name;
-        user.age = input.age;
-        user.save();
-        renderJSON(user);
-    }
+  public static void add(String body) {
+    User input = JSON.parseObject(body, User.class);
+    User user = new User();
+    user.name = input.name;
+    user.age = input.age;
+    user.save();
+    renderJSON(user);
+  }
 
-    public static void delete(Long id) {
-        User user = new User();
-        user = user.findById(id);
-        user.delete();
-        response.status = 204;
-    }
+  public static void delete(Long id) {
+    User user = new User();
+    user = user.findById(id);
+    user.delete();
+    response.status = 204;
+  }
 
-    public static void update(Long id,String body) {
-        User input = JSON.parseObject(body, User.class);
-        User user = new User();
-        user = user.findById(id);
-        user.name = input.name;
-        user.save();
-        renderJSON(user);
-    }
-    public static void list() {
-        renderJSON(User.findAll());
-    }
+  public static void update(Long id, String body) {
+    User input = JSON.parseObject(body, User.class);
+    User user = new User();
+    user = user.findById(id);
+    user.name = input.name;
+    user.save();
+    renderJSON(user);
+  }
+
+  public static void list() {
+    renderJSON(User.findAll());
+  }
 }
